27e336b9a30492d667091e9dba7b3cb49f371caa
Pedro Lima <tsc.pedrolima@gmail.com>
2024-05-21 22:56:47 -0300

    style: add react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { TokenResponse, useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
-import { setCookie } from "nookies";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
 import { useEffect, useState } from "react";
+import { IoPersonCircle } from "react-icons/io5";
 
 interface ProfileType {
   name: string;
@@ -21,9 +22,15 @@ function App() {
     onError: (error) => console.log("Login Failed:", error)
   });
 
+  const logout = () => {
+    destroyCookie(null, "auth_token");
+  };
+
   useEffect(() => {
+    const { "auth_token": token } = parseCookies();
     console.log(user);
     console.log(profile);
+    console.log(token);
     
     if (user) {
       axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
@@ -38,9 +45,21 @@ function App() {
         .catch(error => {
           console.error(error);
         });
-    }
-  }, [ user ]
-  );
+    } else if (token) {
+      axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${token}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json"
+        }
+      }
+      ).then(res => {
+        setProfile(res.data);
+      })
+        .catch(error => {
+          console.error(error);
+        });
+    }  
+  }, [ user ]);
 
   return (
     <main className="bg-zinc-800 w-full h-screen text-white flex justify-center items-center">
@@ -49,18 +68,24 @@ function App() {
           <>
             <h3 className="text-2xl">Olá <strong>{profile.name}</strong></h3>
             <img src={profile.picture} className="rounded-full" alt="Imagem de perfil" />
+            <button 
+              className="px-6 py-2 bg-red-500 hover:bg-red-600 transition-all text-white font-semibold text-lg rounded-full" 
+              onClick={() => logout()}
+            >Sair</button>
           </>
         ) : (
           <>
             <h3 className="text-2xl">Faça login com auxílio do <strong>React Oauth Google</strong></h3>
-            <div className="rounded-full w-[96px] h-[96px] bg-gray-500/30"></div>
+            <div className="rounded-full w-[96px] h-[96px] bg-gray-500/30">
+              <IoPersonCircle className="w-full h-full" />
+            </div>
+            <button 
+              className="px-6 py-2 bg-white hover:bg-gray-200 transition-all text-black font-semibold text-lg rounded-full" 
+              onClick={() => login()}
+            >Entrar com Google</button>
           </>
         )
         }
-        <button 
-          className="px-6 py-2 bg-white hover:bg-gray-200 transition-all text-black font-semibold text-lg rounded-full" 
-          onClick={() => login()}
-        >Entrar com Google</button>
       </div>
     </main>
   );
